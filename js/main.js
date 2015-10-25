010d80971c3125f5a757283551c1125e99ac80db
Jonathan Sofer <jdsofer@gmail.com>
2015-10-24 18:51:47 -0700

    Added Motion Path

--- js/main.js
+++ js/main.js
@@ -8,8 +8,70 @@ function preload() {
 function create() {
 	game.add.image(game.world.centerX, game.world.centerY, 'background').anchor.set(0.49, 0.21);
 	game.add.sprite(0, 0, 'Mario');
+
+	this.bmd = this.add.bitmapData(this.game.width, this.game.height);
+	this.bmd.addToWorld();
+
+	var py = this.points.y;
+	for (var i = 0; i < py.length; i++) {
+		py[i] = this.rnd.between(32, 432);
+	}
 }
 
 function update() {
 
 }
+
+var PhaserGame = function () {
+
+    this.bmd = null;
+
+    this.points = {
+        'x': [ 32, 128, 256, 384, 512, 608 ],
+        'y': [ 240, 240, 240, 240, 240, 240 ]
+    };
+
+};
+
+PhaserGame.prototype = {
+
+    create: function () {
+
+        this.bmd = this.add.bitmapData(this.game.width, this.game.height);
+        this.bmd.addToWorld();
+
+        var py = this.points.y;
+
+        for (var i = 0; i < py.length; i++)
+        {
+            py[i] = this.rnd.between(32, 432);
+        }
+
+        this.plot();
+
+    },
+
+    plot: function () {
+
+        this.bmd.clear();
+
+        var x = 1 / game.width;
+
+        for (var i = 0; i <= 1; i += x)
+        {
+            var px = this.math.catmullRomInterpolation(this.points.x, i);
+            var py = this.math.catmullRomInterpolation(this.points.y, i);
+
+            this.bmd.rect(px, py, 1, 1, 'rgba(255, 255, 255, 1)');
+        }
+
+        for (var p = 0; p < this.points.x.length; p++)
+        {
+            this.bmd.rect(this.points.x[p]-3, this.points.y[p]-3, 6, 6, 'rgba(255, 0, 0, 1)');
+        }
+
+    }
+
+};
+
+game.state.add('Game', PhaserGame, true);
