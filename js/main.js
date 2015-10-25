5bfddaa9712b9e03c7f532aca837cc77a49e59c9
Jonathan Sofer <jdsofer@gmail.com>
2015-10-24 18:35:39 -0700

    Added Mario for Testing

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameWindow', { preload: prelo
 
 function preload() {
 	game.load.image('background', 'assets/space.jpg');
+	game.load.image('Mario', 'assets/Mario.png');
 }
 
 function create() {
-	bg = game.add.sprite(0, 0, 50, 100, 'background');
+	game.add.image(game.world.centerX, game.world.centerY, 'background').anchor.set(0.49, 0.21);
+	game.add.sprite(0, 0, 'Mario');
 }
 
 function update() {
