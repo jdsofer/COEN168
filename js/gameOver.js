6889981159da15004482258cad564dd954b41ee8
Jonathan Sofer <jdsofer@gmail.com>
2015-12-02 16:16:05 -0800

    Added client side ajax for player score post

--- js/gameOver.js
+++ js/gameOver.js
@@ -5,6 +5,7 @@ gameOver.prototype={
 		this.score = score;
 	},
 	create: function(){
+
 		this.game.add.image(this.game.world.centerX, this.game.world.centerY, "background").anchor.set(0.6, 0.5);
   		var gameOverText=this.game.add.text(400, 200, "Game Over", {font: "35px Arial", fill: "#000000", align: "center"});
 		gameOverText.anchor.setTo(0.5, 0.5);
@@ -14,6 +15,24 @@ gameOver.prototype={
 		playAgainButton.anchor.setTo(0.5,0.5);
 		var gameMenuButton=this.game.add.button(400, 420, "gameMenu", this.launchGameMenu);
 		gameMenuButton.anchor.setTo(0.5,0.5);
+
+		var person = prompt("Enter your name!");
+		var data = {"player": person,
+					"score": this.score 
+		}
+
+		console.log(data);
+
+    	if (person != null) {
+        	$.ajax({
+  				type: "POST",
+  				contentType: "application/json",
+  				async: false,
+  				url: "../index.php",
+  				data: JSON.stringify(data)
+			});
+    	}
+
 	},
 	launchTheGame: function(){
 		this.game.state.start("TheGame");
